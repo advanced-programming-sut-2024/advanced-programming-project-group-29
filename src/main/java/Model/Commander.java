f3aa7de7081c9b307848945775e70cae80ce10d1
mahdiostad04 <ostadmohammadimahdi@gmail.com>
2024-05-31 01:23:34 +0330

    complete User Class

--- src/main/java/Model/Commander.java
+++ src/main/java/Model/Commander.java
@@ -1,5 +1,23 @@
 package Model;
 
-public class Commander extends Card { // Ostad
+public class Commander extends Card {
+    private boolean remains;
 
+    private static Runnable getExecuteActionByCommanderName(String commanderName) {
+        //TODO
+        return new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        };
+    }
+
+    public void executeAction() {
+
+    }
+
+    public boolean isRemains() {
+        return remains;
+    }
 }
--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -1,4 +1,148 @@
 package Model;
 
-public class User { // Ostad
+import java.util.ArrayList;
+import java.util.Random;
+
+public class User {
+    private String username;
+    private String password;
+    private String nickname;
+    private String email;
+    private final ArrayList<Card> hand = new ArrayList<>();
+    private final ArrayList<Card> deck = new ArrayList<>();
+    private final ArrayList<Card> discardPile = new ArrayList<>();
+    private Faction faction;
+    private Commander commander;
+    private static ArrayList<User> allUsers = new ArrayList<>();
+    private static User currentUser;
+
+    public User(String username, String password, String nickname, String email) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+        this.email = email;
+        allUsers.add(this);
+    }
+
+    public static ArrayList<User> getAllUsers() {
+        return allUsers;
+    }
+
+    public static void setAllUsers(ArrayList<User> allUsers) {
+        User.allUsers = allUsers;
+    }
+
+    public static User getCurrentUser() {
+        return currentUser;
+    }
+
+    public static void setCurrentUser(User currentUser) {
+        User.currentUser = currentUser;
+    }
+
+    public static User getUserByUsername(String name) {
+        for (User u : allUsers) if (u.getUsername().equals(name)) return u;
+        return null;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Faction getFaction() {
+        return faction;
+    }
+
+    public void setFaction(Faction faction) {
+        this.faction = faction;
+    }
+
+    public Commander getCommander() {
+        return commander;
+    }
+
+    public void setCommander(Commander commander) {
+        this.commander = commander;
+    }
+
+    public void addCardToHand(Card card) {
+        this.hand.add(card);
+    }
+
+    public void removeCardFromHand(Card card) {
+        this.hand.remove(card);
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public void addCardToDeck(Card card) {
+        this.deck.add(card);
+    }
+
+    public void removeCardFromDeck(Card card) {
+        this.deck.remove(card);
+    }
+
+    public ArrayList<Card> getDeck() {
+        return deck;
+    }
+
+    public Card getCardFromDeckWithNumber(int number) {
+        return deck.get(number);
+    }
+
+    public Card getCardFromDeckRandomly() {
+        return deck.get((new Random()).nextInt(deck.size()));
+    }
+
+    public void replaceCardFromDeck(Card card) {
+        int index = this.hand.indexOf(card);
+        Card replaceCard = getCardFromDeckRandomly();
+        this.hand.remove(card);
+        this.hand.set(index, replaceCard);
+        this.deck.remove(replaceCard);
+        this.deck.add(card);
+    }
+
+    public void addCardToDiscardPile(Card card) {
+        this.discardPile.add(card);
+    }
+
+    public void removeCardFromDiscardPile(Card card) {
+        this.discardPile.remove(card);
+    }
+
+    public ArrayList<Card> getDiscardPile() {
+        return discardPile;
+    }
 }
