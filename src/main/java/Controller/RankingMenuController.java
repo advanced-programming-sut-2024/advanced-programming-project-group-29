e1596dd37143814f1ec27fdb6ce6eb87edd750f6
radalj <radinjarireh@gmail.com>
2024-07-01 11:44:29 +0330

    Ranking Controller Output reformatted

--- src/main/java/Controller/RankingMenuController.java
+++ src/main/java/Controller/RankingMenuController.java
@@ -5,22 +5,26 @@ import Model.User;
 import java.util.ArrayList;
 
 public class RankingMenuController {
-    public static ArrayList<String> getRanking() {
+    public static ArrayList<ArrayList<String> > getRanking() {
         ArrayList<String> users = User.getAllUsersByRank();
-        ArrayList<String> result = new ArrayList<>();
+        ArrayList<ArrayList<String> > result = new ArrayList<>();
+        ArrayList<String> temporary = new ArrayList<>();
         User First = User.getUserByUsername(users.get(0));
-        result.add("1. " + First.getUsername() + " " + First.getNumberOfWins());
+        temporary.add("1");
+        temporary.add(First.getUsername());
+        temporary.add(String.valueOf(First.getNumberOfWins()));
+        result.add(temporary);
         int currentRank = 1;
         int currentWins = First.getNumberOfWins();
         for (int i = 1; i < users.size(); i++) {
+            temporary.clear();
             User user = User.getUserByUsername(users.get(i));
-            if (user.getNumberOfWins() == currentWins) {
-                result.add(currentRank + ". " + user.getUsername() + " " + user.getNumberOfWins());
-            } else {
+            if (user.getNumberOfWins() != currentWins)
                 currentRank = i + 1;
-                currentWins = user.getNumberOfWins();
-                result.add(currentRank + ". " + user.getUsername() + " " + currentWins);
-            }
+            temporary.add(String.valueOf(currentRank));
+            temporary.add(user.getUsername());
+            temporary.add(String.valueOf(user.getNumberOfWins()));
+            result.add(temporary);
         }
         return result;
     }
