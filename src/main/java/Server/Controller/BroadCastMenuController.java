0f605e641702a38bf9e47fd57aef13645b94f8e2
fatemetmhr <fatemetmhr@gmail.com>
2024-07-11 16:51:17 +0330

    Commands to get game logs separately added

--- src/main/java/Server/Controller/BroadCastMenuController.java
+++ src/main/java/Server/Controller/BroadCastMenuController.java
@@ -1,6 +1,6 @@
 package Server.Controller;
 
-import Server.Model.GameHistory;
+import Server.Model.*;
 import Server.Model.Sender;
 import Server.Model.User;
 import Server.Regex.BroadCastMenuRegex;
@@ -13,8 +13,12 @@ public class BroadCastMenuController {
         try {
             Object result = null;
             Matcher matcher;
-            if((matcher = BroadCastMenuRegex.GET_GAME_HISTORY.getMatcher(inputCommand)).matches()){
-                result = getGameHistory(matcher.group("username"));
+            if((matcher = BroadCastMenuRegex.GET_GAME_LOG_SIZE.getMatcher(inputCommand)).matches()){
+                result = getGameLogSize(matcher.group("username"), Integer.parseInt(matcher.group("gameNumber")));
+            } else if((matcher = BroadCastMenuRegex.GET_GAME_LOG_COMMAND.getMatcher(inputCommand)).matches()){
+                result = getGameLogCommand(matcher.group("username"), Integer.parseInt(matcher.group("gameNumber")), Integer.parseInt(matcher.group("index")));
+            } else if((matcher = BroadCastMenuRegex.GET_GAME_LOG_GAME_BOARDIN.getMatcher(inputCommand)).matches()){
+                result = getGameLogGameBoardin(matcher.group("username"), Integer.parseInt(matcher.group("gameNumber")), Integer.parseInt(matcher.group("index")));
             }
             return result;
         } catch (Exception e) {
@@ -23,11 +27,23 @@ public class BroadCastMenuController {
         }
     }
 
-    public static GameHistory getGameHistory(String username) {
-        User user = User.getUserByUsername(username);
-        System.err.println("username : "+user.getUsername());
-        System.err.println(user.getGameHistory().getLast());
-        return user.getGameHistory().getLast();
+    private static GameBoardin getGameLogGameBoardin(String username, int gameNumber, int index) {
+        int gameHistorySize = User.getUserByUsername(username).getGameHistory().size();
+        GameLog gameLog = User.getUserByUsername(username).getGameHistory().get(gameHistorySize - gameNumber - 1).getGameLog();
+        return gameLog.getGameBoardins().get(index);
     }
+
+    private static String getGameLogCommand(String username, int gameNumber, int index) {
+        int gameHistorySize = User.getUserByUsername(username).getGameHistory().size();
+        GameLog gameLog = User.getUserByUsername(username).getGameHistory().get(gameHistorySize - gameNumber - 1).getGameLog();
+        return gameLog.getCommands().get(index);
+    }
+
+    private static Integer getGameLogSize(String username, int gameNumber) {
+        int gameHistorySize = User.getUserByUsername(username).getGameHistory().size();
+        GameLog gameLog = User.getUserByUsername(username).getGameHistory().get(gameHistorySize - gameNumber - 1).getGameLog();
+        return gameLog.getCommands().size();
+    }
+
 }
 
